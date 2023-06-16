5064b54f74520d9ce50e10c02a3cfae2baedba4c
andreshungbz <andres.hung@outlook.com>
2023-06-15 22:00:48 -0600

    yet another test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,7 +1,10 @@
 import Prompt from '@models/prompt';
 import { connectToDB } from '@utils/database';
+import { headers } from 'next/headers';
 
 export const GET = async (request) => {
+  const headersList = headers();
+  const referer = headersList.get('referer');
   try {
     await connectToDB();
 
@@ -9,6 +12,7 @@ export const GET = async (request) => {
     return new Response(JSON.stringify(prompts), {
       status: 200,
       headers: {
+        referer: referer,
         'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
       },
     });
