1ec695b065a1c55e64855e81aa1445b6494136b5
andreshungbz <andres.hung@outlook.com>
2023-06-15 22:12:03 -0600

    fixed? STOP Vercel from caching Feed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,6 +3,8 @@ import { connectToDB } from '@utils/database';
 import { headers } from 'next/headers';
 
 export const GET = async (request) => {
+  const headersList = headers();
+  const referer = headersList.get('referer');
   try {
     await connectToDB();
 
@@ -10,6 +12,7 @@ export const GET = async (request) => {
     return new Response(JSON.stringify(prompts), {
       status: 200,
       headers: {
+        referer: referer,
         'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
       },
     });
